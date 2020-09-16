221a9504055d7a928514105b9f043f3be2c7171c
glorroll <62908757+glorroll@users.noreply.github.com>
2020-09-16 17:46:01 +0900

    querySelector

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const number = document.getElementById("number");
 const increase = document.getElementById("increase");
 const decrease = document.getElementById("decrease");
 
-/*console.log(number.innerText);
+console.log(number.innerText);
 console.log(increase.offsetTop);
-console.log(decrease.id); */
+console.log(decrease.id);
 
 increase.onclick = () => {
   const current = parseInt(number.innerText, 10);
@@ -16,3 +16,23 @@ decrease.onclick = () => {
   const current = parseInt(number.innerText, 10);
   number.innerText = current - 1;
 };
+/*
+
+// id가 없을 경우 querySelector
+const number = document.getElementById("number");
+const buttons = document.querySelectorAll('button');
+// id가 아닌 tag이름으로 찾아 냄
+console.log(buttons);
+const [increase, decrease] = buttons;
+// increase 와 decrease를 buttons에서 추출해서 작업 처리
+
+increase.onclick = () => {
+  const current = parseInt(number.innerText, 10);
+  number.innerText = current + 1;
+};
+
+decrease.onclick = () => {
+  const current = parseInt(number.innerText, 10);
+  number.innerText = current - 1;
+};
+*/
